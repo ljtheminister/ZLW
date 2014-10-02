a8fa109f8863c1cf9f30f723cfe8245c8e774eae
ljtheminister <john.c.min@gmail.com>
2014-10-02 12:28:14 -0400

    pretty much done and would work if i use forloop instead of apply(data, 1, checkExtreme)

--- Problem1a.R
+++ Problem1a.R
@@ -1,10 +1,33 @@
+library(dplyr)
+library(data.table)
+
 setwd('/home/lj/ML/Zillow/')
 dat <- read.csv("Transactions.csv")
 
 nrw <- nrow(dat)
 extremeVals <- list()
 
-region_ids <- group_by(dat, regionid)
+data_by_region <- group_by(dat, regionid)
+means <- summarise(data_by_region, transvalue=mean(transvalue, na.rm=TRUE), bathroomcnt=mean(bathroomcnt, na.rm=TRUE), bedroomcnt=mean(bedroomcnt, na.rm=TRUE), finishedsquarefeet=mean(finishedsquarefeet, na.rm=TRUE), lotsizesquarefeet=mean(lotsizesquarefeet, na.rm=TRUE))
+sds <- summarise(data_by_region, transvalue=sd(transvalue, na.rm=TRUE), bathroomcnt=sd(bathroomcnt, na.rm=TRUE), bedroomcnt=sd(bedroomcnt, na.rm=TRUE), finishedsquarefeet=sd(finishedsquarefeet, na.rm=TRUE), lotsizesquarefeet=sd(lotsizesquarefeet, na.rm=TRUE))
+rownames(means) <- means$regionid
+rownames(sds) <- sds$regionid
+means <- means[, 2:6]
+sds <- sds[,2:6]
+col_names <- c('transvalue', 'bathroomcnt', 'bedroomcnt', 'finishedsquarefeet', 'lotsizesquarefeet')
 
-means <- summarise(region_ids, mean(transvalue, na.rm=TRUE), mean(bathroomcnt, na.rm=TRUE), mean(bedroomcnt, na.rm=TRUE), mean(finishedsquarefeet, na.rm=TRUE), mean(lotsizesquarefeet, na.rm=TRUE))
-sds <- summarise(region_ids, std(transvalue, na.rm=TRUE), std(bathroomcnt, na.rm=TRUE), std(bedroomcnt, na.rm=TRUE), std(finishedsquarefeet, na.rm=TRUE), std(lotsizesquarefeet, na.rm=TRUE))
+dat$isExtreme=FALSE
+
+checkExtreme <- function(x) {  
+  regionid <- as.character(x['regionid'])
+  diff <- abs(x[col_names] - means[regionid,])
+  extreme = diff > 2*sds[regionid,]
+  if (any(extreme==TRUE)) {
+    x$isExtreme = TRUE
+  }
+}
+
+checkExtreme(tmp)
+
+apply(dat, 1, checkExtreme)
+
